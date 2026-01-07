f11491a7390c136dd6287e569172c9ca1fedeecd
LeeSW <tmddnr3163@gmail.com>
2026-01-08 00:45:28 +0900

    나의 첫번쨰 블록체인

--- namlulucoin/main.go
+++ namlulucoin/main.go
@@ -11,10 +11,40 @@ type block struct {
 	prevHash string
 }
 
-func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) == 0 {
+		return ""
+	}
+	return b.blocks[len(b.blocks)-1].hash
+}
+
+func (b *blockchain) listBlocks() []block {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Previous Hash: %s\n", block.prevHash)
+		fmt.Println()
+	}
+	return b.blocks
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
 	hexHash := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hexHash
-	fmt.Println(genesisBlock)
+	newBlock.hash = hexHash
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func main() {
+	chain := blockchain{}
+	chain.addBlock("First Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+
+	chain.listBlocks()
 }
